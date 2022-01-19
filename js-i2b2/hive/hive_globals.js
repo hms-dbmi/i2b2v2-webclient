f887f539a1fe9fb897e569f0f3bf30b89570e273
Nick Benik <nbenik@gmail.com>
2022-01-19 10:48:44 -0500

    Template updates for CRC Query Tool

--- js-i2b2/hive/hive_globals.js
+++ js-i2b2/hive/hive_globals.js
@@ -229,5 +229,25 @@ function i2b2_BaseCell(configObj) {
         .catch(console.log.bind(console));
 }
 
+
+// ================= handlebars helper to manage SELECT option selection ==================
+Handlebars.registerHelper('select', function(value, options) {
+    // Create a select element
+    var select = document.createElement('select');
+
+    // Populate it with the option HTML
+    select.innerHTML = options.fn(this);
+
+    // Set the value
+    select.value = value;
+
+    // Find the selected node, if it exists, add the selected attribute to it
+    if (select.children[select.selectedIndex])
+        select.children[select.selectedIndex].setAttribute('selected', 'selected');
+
+    return select.innerHTML;
+});
+
+
 console.timeEnd('execute time');
 console.groupEnd();
