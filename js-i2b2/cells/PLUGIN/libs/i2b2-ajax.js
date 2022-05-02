d09dfc8df423515d5630e9e4d203e61eb44f2182
marc-danie <marc-danie_nazaire@hms.harvard.edu>
2022-05-02 14:19:29 -0400

    I2B2UI-102 - Coding of communication layer - stub code

--- js-i2b2/cells/PLUGIN/libs/i2b2-ajax.js
+++ js-i2b2/cells/PLUGIN/libs/i2b2-ajax.js
@@ -2,3 +2,24 @@
  * This file is the library that is loaded into a plugin to allow
  * communications with the i2b2 cells.
  **/
+
+i2b2 = {};
+i2b2.plugin = {};
+i2b2.plugin.lib = {};
+i2b2.plugin.lib.routeFunc = function(cell, funcName, params){
+    alert("routing function cell " + cell + "\tfuncName:\t" + funcName + "\tparams\t" + params);
+}
+
+i2b2.plugin.lib.init = function(cellFuncMap) {
+  let cells = Object.keys(cellFuncMap);
+
+  cells.forEach(cell => {
+      let functionNames = cellFuncMap[cell];
+      i2b2[cell]  = {};
+      functionNames.forEach(func => {
+          i2b2[cell][func] = function(params) {
+              i2b2.plugin.lib.routeFunc(cell, func, params);
+          }
+      })
+  });
+};
