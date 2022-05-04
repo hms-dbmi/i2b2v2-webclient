ef020832d51a1d88764fd9a473e08164ae5661ff
marc-danie <marc-danie_nazaire@hms.harvard.edu>
2022-05-04 15:26:47 -0400

    I2B2UI-102 - Coding of communication layer - init ajax functions in ajax library

--- js-i2b2/cells/PLUGIN/libs/i2b2-ajax.js
+++ js-i2b2/cells/PLUGIN/libs/i2b2-ajax.js
@@ -2,8 +2,6 @@
  * This file is the library that is loaded into a plugin to allow
  * communications with the i2b2 cells.
  **/
-
-i2b2 = {};
 i2b2.plugin = {};
 i2b2.plugin.lib = {};
 i2b2.plugin.lib.routeFunc = function(cell, funcName, params){
@@ -12,8 +10,7 @@ i2b2.plugin.lib.routeFunc = function(cell, funcName, params){
 
 i2b2.plugin.lib.init = function(cellFuncMap) {
   let cells = Object.keys(cellFuncMap);
-
-  cells.forEach(cell => {
+    cells.forEach(cell => {
       let functionNames = cellFuncMap[cell];
       i2b2[cell]  = {};
       functionNames.forEach(func => {
@@ -26,6 +23,9 @@ i2b2.plugin.lib.init = function(cellFuncMap) {
 
 //======================================================================================================================
 window.addEventListener("I2B2_INIT_AJAX", function(evt) {
-    console.log("Initialize AJAX");
-    console.dir(evt.detail);
+    console.log("Initializing AJAX");
+    i2b2.plugin.lib.init(evt.detail);
+
+    //try running a typical function in i2b2
+    i2b2.PM.getUserAuth("test");
 });
