82b1b0245377b5650f0ee32dbe82168e5591c4ac
marc-danie <marc-danie_nazaire@hms.harvard.edu>
2022-05-04 16:05:53 -0400

    I2B2UI-102 - Coding of communication layer - merged ajax code

--- js-i2b2/cells/PLUGIN/libs/i2b2-ajax.js
+++ js-i2b2/cells/PLUGIN/libs/i2b2-ajax.js
@@ -2,25 +2,6 @@
  * This file is the library that is loaded into a plugin to allow
  * communications with the i2b2 cells.
  **/
-i2b2.plugin = {};
-i2b2.plugin.lib = {};
-i2b2.plugin.lib.routeFunc = function(cell, funcName, params){
-    alert("routing function cell " + cell + "\tfuncName:\t" + funcName + "\tparams\t" + params);
-}
-
-i2b2.plugin.lib.init = function(cellFuncMap) {
-  let cells = Object.keys(cellFuncMap);
-    cells.forEach(cell => {
-      let functionNames = cellFuncMap[cell];
-      i2b2[cell]  = {};
-      functionNames.forEach(func => {
-          i2b2[cell][func] = function(params) {
-              i2b2.plugin.lib.routeFunc(cell, func, params);
-          }
-      })
-  });
-};
-
 i2b2.ajax = {};
 i2b2.ajax._msgDb = {};
 
@@ -93,12 +74,4 @@ window.addEventListener("message", (event) => {
     if (event.data.msgType === "AJAX_REPLY" || event.data.msgType === "AJAX_ERROR") {
         i2b2.ajax._receiveMsg(event.data);
     }
-});
-
-window.addEventListener("I2B2_INIT_AJAX", function(evt) {
-    console.log("Initializing AJAX");
-    i2b2.plugin.lib.init(evt.detail);
-
-    //try running a typical function in i2b2
-    i2b2.PM.getUserAuth("test");
 });
