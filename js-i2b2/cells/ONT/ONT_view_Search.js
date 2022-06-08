6b7e4cf46667bf1ab2c693809309e93b5e145201
marc-danie <marc-danie_nazaire@hms.harvard.edu>
2022-06-08 10:11:29 -0400

    I2B2UI-139 - Search input field must contains 3 char; add logic on the search button - Moved Search related logic to separate js file

---
+++ js-i2b2/cells/ONT/ONT_view_Search.js
@@ -0,0 +1,140 @@
+/**
+ * @projectDescription	View controller for ONT's "Term Search" .
+ * @inherits 	i2b2.ONT.view
+ * @namespace	i2b2.ONT.view.search
+ * @author		Marc-Danie
+ * @version 	2.0
+ * ----------------------------------------------------------------------------------------
+ */
+i2b2.ONT.view.search = {};
+
+i2b2.ONT.view.search.toggleSearchOptions = function(elem){
+    let currentVal = $(elem).val();
+    if(currentVal === "coding"){
+        $("#categoryOptions").addClass("hidden");
+        $("#codingOptions").removeClass("hidden");
+    }
+    else{
+        $("#categoryOptions").removeClass("hidden");
+        $("#codingOptions").addClass("hidden");
+    }
+};
+
+//================================================================================================== //
+
+i2b2.ONT.view.search.toggleSearchClearIcon = function(){
+    let currentVal = $("#searchTermText").val();
+    if(currentVal){
+        $("#searchTerm .clearIcon").removeClass("hidden");
+    }
+    else{
+        $("#searchTerm .clearIcon").addClass("hidden");
+    }
+};
+
+//================================================================================================== //
+
+i2b2.ONT.view.search.clearSearchInput = function(){
+    $("#searchTermText").val("");
+    $("#searchTerm .clearIcon").addClass("hidden");
+};
+
+//================================================================================================== //
+
+i2b2.ONT.view.search.initSearch = function(container){
+    // Load the finder templatee
+    $.ajax("js-i2b2/cells/ONT/assets/OntologyFinder.html", {
+        success: (template) => {
+            let finderTemplate = Handlebars.compile(template);
+            $(finderTemplate({})).prependTo(container);
+        },
+        error: (error) => { console.error("Could not retrieve template: OntologyFinder.html"); }
+    });
+};
+
+//================================================================================================== //
+
+i2b2.ONT.view.search.initSearchCatOptions = function(){
+    $.ajax("js-i2b2/cells/ONT/templates/OntologyFinderFilterOptions.html", {
+        success: (template) => {
+            let categoryOptions = Handlebars.compile(template);
+            let categories = [];
+            for (let i=0; i<i2b2.ONT.model.Categories.length; i++) {
+                let cat = i2b2.ONT.model.Categories[i];
+                let catVal = cat.key.substring(2,cat.key.indexOf('\\',3))
+                categories.push({
+                    name: cat.name,
+                    value: catVal
+                });
+            }
+            let options = {
+                "option": categories
+            };
+            $(categoryOptions(options)).appendTo("#categorySubmenu");
+
+            $("#liCat").hover(function(){
+                $("#codingSubmenu").hide().closest("li").removeClass("highlight-menu-item");
+                $("#categorySubmenu").css("left", "100%").show();
+            });
+
+            $("#liCoding").hover(function(){
+
+                $("#categorySubmenu").hide().closest("li").removeClass("highlight-menu-item");
+                $("#codingSubmenu").css("left", "100%").show();
+            });
+
+            $(".submenu li").on("click", function(){
+                $(".active").removeClass("active");
+                let newDisplayText = $(this).find("button").addClass("active").text();
+                let newValue = $(this).find("button").addClass("active").data("search-filter-value");
+                $("#searchFilterText").text(newDisplayText);
+                $("#searchFilter").data("selected-filter", newValue);
+            });
+
+            $("#searchActions .reset").click(function() {
+                $("#searchTermText").val("");
+            });
+
+            $('#i2b2FinderOnt .navbar-nav').on('shown.bs.dropdown', function () {
+                let navBarMain = $("#i2b2FinderOnt .navbarMain .active")
+                let parentMenu = navBarMain.closest(".submenu").closest("li");
+                navBarMain.closest(".submenu").each(function(){
+                    $(this).css("left", parentMenu.width());
+                    $(this).show();
+                }).closest("li").addClass("highlight-menu-item");
+            });
+        },
+        error: (error) => { console.error("Could not retrieve template: OntologyFinderFilterOption.html"); }
+    });
+
+    $('#i2b2FinderOnt .navbar-nav').on('show.bs.dropdown', function () {
+        $(".submenu").hide();
+        $("#i2b2FinderOnt .navbarMain .active").closest(".submenu").each(function(){
+            $(this).show();
+        }).closest("li").addClass("highlight-menu-item");
+    });
+};
+
+//================================================================================================== //
+
+i2b2.ONT.view.search.initCodingSysOptions = function(){
+    $.ajax("js-i2b2/cells/ONT/templates/OntologyFinderFilterOptions.html", {
+        success: (template) => {
+            let codingSystemOptions = Handlebars.compile(template);
+            let codingSystems = [];
+            for (let i=0; i<i2b2.ONT.model.Schemes.length; i++) {
+                let cat = i2b2.ONT.model.Schemes[i];
+                codingSystems.push({
+                    name: cat.name,
+                    value: cat.key
+                });
+            }
+            let options = {
+                "option": codingSystems
+            };
+            $(codingSystemOptions(options)).appendTo("#codingSubmenu");
+        },
+        error: (error) => { console.error("Could not retrieve template: OntologyFinderCodingOptions.html"); }
+    });
+}
+//================================================================================================== //
