2d4ccc595ef5c62679f9e26bccc747f03aab693d
Nick Benik <nbenik@gmail.com>
2022-07-07 10:01:53 -0400

    Give more information on messages received from unknown windows

--- js-i2b2/cells/PLUGIN/PLUGIN_ctrlr_general.js
+++ js-i2b2/cells/PLUGIN/PLUGIN_ctrlr_general.js
@@ -191,6 +191,7 @@ i2b2.events.afterAllCellsLoaded.add((function() {
             }
             if (foundInstance === undefined) {
                 console.warn("PluginMgr: MESSAGE FROM UNKNOWN WINDOW");
+                console.dir(event);
                 return false;
             }
 
