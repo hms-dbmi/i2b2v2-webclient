2406c866638541e9251026c0c7ffc0ddec00c2b8
Nick Benik <nbenik@gmail.com>
2022-09-29 12:40:10 -0400

    I2B2UI-228: Remove default user/password settings/labels to pass sec scan

--- js-i2b2/i2b2_ui_config.js
+++ js-i2b2/i2b2_ui_config.js
@@ -12,14 +12,8 @@
 i2b2.UI = {};
 /* Start Configuration. Note: be careful to keep trailing commas after each parameter */
 i2b2.UI.cfg = {
-
-    titleLogoSrc : "assets/images/title.gif", /* [Default: "assets/images/title.gif"] 246x26 pixel logo */
-    loginHeaderText : "i2b2 Login",  /* [Default: "i2b2 Login"] Text in the header bar of the login box */
-    loginUsernameText : "Username:", /* [Default: "Username:"] Text for the username input of the login box */
-    loginDefaultUsername : "demo", /* [Default: "demo"] Default text inside the username input */
-    loginPasswordText : "Password:", /* [Default: "Password:"] Text for the password input of the login box */
-    loginDefaultPassword : "demouser", /* [Default: "demouser"] Default text inside the password input */
-    loginHostText : "i2b2 Host:", /* [Default: "i2b2 Host:"] Text for the PM cell dropdown of the login box */
+    // loginDefaultUsername : "demo", /* [Default: "demo"] Default text inside the username input */
+    // loginDefaultPassword : "demouser", /* [Default: "demouser"] Default text inside the password input */
     obfuscatedDisplayNumber: 3, /* [Default: 3] Display number after plus/minus for obfuscated results.
                                   Control the real obfuscation value from server in CRC properties. */
     useFloorThreshold: false, /* [Default: false] If true, any result below floorThresholdNumber shows as 'Less Than {floorThresholdNumber}' */
