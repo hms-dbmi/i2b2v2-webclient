1d6961afb539c48bf36ba53fa8e5f348c89ee67d
marc-danie <marc-danie_nazaire@hms.harvard.edu>
2023-04-06 19:37:43 -0400

    I2B2UI-355 - Modifiers > "undefined" is displayed instead of actual selected values in query builder in case user selects more than 1 Enum value for a Modifier

--- js-i2b2/cells/CRC/CRC_view_QryTool.js
+++ js-i2b2/cells/CRC/CRC_view_QryTool.js
@@ -1157,7 +1157,7 @@ i2b2.CRC.view.QT.updateModifierDisplayValue = function(sdxConcept, extractedLabV
             modifierInfoText  = "= "  + sdxConcept.LabValues.flagValue;
         }
         else if(sdxConcept.LabValues.isEnum){
-            modifierInfoText  = "= " + extractedLabValues.enumInfo[sdxConcept.LabValues.value];
+            modifierInfoText  = "= " + sdxConcept.LabValues.value.map(x => ' "' + extractedLabValues.enumInfo[x] + '"');
         }
         else if (sdxConcept.LabValues.valueType === i2b2.CRC.ctrlr.labValues.VALUE_TYPES.NUMBER){
             let numericOperatorMapping = {
