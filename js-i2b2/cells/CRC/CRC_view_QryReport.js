6becd64e4d6b4ce8937907ef9554c066b695b5ea
marc-danie <marc-danie_nazaire@hms.harvard.edu>
2024-05-15 14:48:42 -0400

    I2B2UI-799 - Fix display of QueryMasterID Manager Data Exporter

--- js-i2b2/cells/CRC/CRC_view_QryReport.js
+++ js-i2b2/cells/CRC/CRC_view_QryReport.js
@@ -35,7 +35,7 @@ i2b2.CRC.view.QueryReport = {
 
                 if (rec.QRS_DisplayType === "CATNUM") {
                     let scopedCallbackQRSI = new i2b2_scopedCallback(this._handleQueryResultInstance);
-                    i2b2.CRC.ajax.getQueryResultInstanceList_fromQueryResultInstanceId("CRC:QueryReport", {qr_key_vGalue: rec.QRS_ID}, scopedCallbackQRSI);
+                    i2b2.CRC.ajax.getQueryResultInstanceList_fromQueryResultInstanceId("CRC:QueryReport", {qr_key_value: rec.QRS_ID}, scopedCallbackQRSI);
                 } else if ((rec.QRS_DisplayType === "LIST")) {
                     //----i2b2.CRC.view.QS.dispDIV.innerHTML += "<div style=\"clear: both; padding-top: 10px; font-weight: bold;\">" + rec.QRS_Description + "</div>";
                 }
