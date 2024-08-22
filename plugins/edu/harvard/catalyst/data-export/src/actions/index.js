dd8f8170db033bd6397b808dde71bb38b7dfc2e4
marc-danie <marc-danie_nazaire@hms.harvard.edu>
2024-08-22 14:56:24 -0400

    Fixed action names

--- plugins/edu/harvard/catalyst/data-export/src/actions/index.js
+++ plugins/edu/harvard/catalyst/data-export/src/actions/index.js
@@ -3,5 +3,5 @@ export const SAVE_TABLE = "saveTable";
 export const TABLE_LISTING = "tableListing";
 
 export const LOAD_DATA_TABLE = `${TABLE_DEF}/loadTable`;
-export const SAVE_DATA_TABLE = `${SAVE_TABLE}/saveTableAction`;
-export const LIST_TABLES = `${TABLE_LISTING }/listTablesAction`;
+export const SAVE_DATA_TABLE = `${SAVE_TABLE}/saveTable`;
+export const LIST_TABLES = `${TABLE_LISTING }/listTables`;
