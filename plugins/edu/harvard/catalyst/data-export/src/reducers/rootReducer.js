4326343c58e8c82d19ad1ecba3cdd3ee57157297
marc-danie <marc-danie_nazaire@hms.harvard.edu>
2024-08-22 07:15:47 -0400

    add saveTableReducer to rootReducer

--- plugins/edu/harvard/catalyst/data-export/src/reducers/rootReducer.js
+++ plugins/edu/harvard/catalyst/data-export/src/reducers/rootReducer.js
@@ -1,7 +1,9 @@
 import loadTableReducer from './loadTableSlice';
+import saveTableReducer from './saveTableSlice';
 
 const rootReducers = {
     dataTable: loadTableReducer,
+    saveTable: saveTableReducer
 };
 
 export default rootReducers;
