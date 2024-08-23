e8c76d5d0cc5e0f216a2fe2f0b1504d7d76ceb10
Nick Benik <nbenik@gmail.com>
2024-08-23 10:23:59 -0400

    Add AuthorizedTunnel access to credentials needed for API call & save to local model

--- plugins/edu/harvard/catalyst/data-export/src/App.js
+++ plugins/edu/harvard/catalyst/data-export/src/App.js
@@ -24,7 +24,6 @@ export default () => {
 
 // ---------------------------------------------------------------------------------------
 window.addEventListener("I2B2_READY", ()=> {
-    debugger;
     // the i2b2 framework is loaded and ready (including population of i2b2.model namespace)
     if (i2b2.model.tableDef === undefined) {
         i2b2.model.tableDef = {};
@@ -34,4 +33,20 @@ window.addEventListener("I2B2_READY", ()=> {
     // attach the drop handler after initialization
     i2b2.sdx.AttachType("dropTrgt", "CONCPT");
     i2b2.sdx.setHandlerCustom("dropTrgt", "CONCPT", "DropHandler", sdxDropHandler);
+
+    // get the authentication info and save it to our model
+    i2b2.authorizedTunnel.variable["i2b2.PM.model.login_username"].then((username) => {
+        i2b2.model.user = username;
+        i2b2.state.save();
+    });
+    i2b2.authorizedTunnel.variable["i2b2.PM.model.login_project"].then((project) => {
+        i2b2.model.project = project;
+        i2b2.state.save();
+    });
+    i2b2.authorizedTunnel.variable["i2b2.PM.model.login_password"].then((password) => {
+        let s = password.indexOf('SessionKey:');
+        let e = password.lastIndexOf('<');
+        i2b2.model.session = password.substr(s+11,e-s);
+        i2b2.state.save();
+    });
 });
