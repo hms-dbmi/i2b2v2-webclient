c2a37660ccec2e6bf4d1792acd10a760d5299064
marc-danie <marc-danie_nazaire@hms.harvard.edu>
2024-08-23 15:42:05 -0400

    removed duplicate i2b2-loader.js file

--- plugins/edu/harvard/catalyst/data-export/build/service-worker.js
+++ plugins/edu/harvard/catalyst/data-export/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.095c65909697c08b3d6b75b8689584b2.js"
+  "./precache-manifest.f48a611c26787d0707d0190ab3592214.js"
 );
 
 self.addEventListener('message', (event) => {
