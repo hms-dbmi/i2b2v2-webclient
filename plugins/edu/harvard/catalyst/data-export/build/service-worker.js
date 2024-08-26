d9ba33c68f0ed8e787d1fe7378e98e61ee39b88d
marc-danie <marc-danie_nazaire@hms.harvard.edu>
2024-08-26 06:45:21 -0400

    I2B2UI-859 - Create the Make Request and submit the request - Added make request UI - Merged in changes from DATA_EXPORT_PLUGIN branch

--- plugins/edu/harvard/catalyst/data-export/build/service-worker.js
+++ plugins/edu/harvard/catalyst/data-export/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.e4f6ac52d806a7a0935ccda9b5516ed1.js"
+  "./precache-manifest.229c2bb202055d7ec016c1c0c7606d39.js"
 );
 
 self.addEventListener('message', (event) => {
