11f440432410abe98200e36a8b0ad5b1c8e470c2
marc-danie <marc-danie_nazaire@hms.harvard.edu>
2024-09-18 23:02:25 -0400

    Includes I2B2UI-901 - Export plugin > Date when the table definition was saved is always 1 prior to the date when it was saved/edited

--- plugins/edu/harvard/catalyst/data-export/src/sagas/listTablesSaga.js
+++ plugins/edu/harvard/catalyst/data-export/src/sagas/listTablesSaga.js
@@ -1,66 +1,26 @@
 /* globals i2b2 */
 
 import { call, takeLatest, put} from "redux-saga/effects";
-/*import { PayloadAction } from "@reduxjs/toolkit";*/
 import {listTablesSuccess, listTablesError} from "../reducers/listTablesSlice";
+import { DateTime } from "luxon";
 
 import {
     LIST_TABLES
 } from "../actions";
 
-const exampleResponse = {
-        "tables": [
-            {
-                "id": "global_8d26d1b6-34e1-4b35-a65c-f8299f802565",
-                "title": "Global Medications Template",
-                "create_date": "2024-01-02",
-                "edit_date": "2024-08-22",
-                "column_count": 7,
-                "user_id": "global",
-                "shared": true
-            },
-            { id:'demo_71e11f05-dd9b-41d7-90ae-ae01bff13b00', user_id:"demo", shared: false, title: "Nick's 1st Demographics run", create_date: "01/01/23", edit_date: "07/07/24", column_count: 10},
-            { id:'demo_71e11f05-dd9b-41d7-90ae-ae01bff13b01', user_id:"demo", shared: false, title: "Dummy test", create_date: "04/05/23", edit_date: "07/07/24", column_count: 20},
-            { id:'demo_71e11f05-dd9b-41d7-90ae-ae01bff13b02', user_id:"demo", shared: false, title: "Example 1", create_date: "06/07/23", edit_date: "07/07/24", column_count: 25},
-            { id:'demo_71e11f05-dd9b-41d7-90ae-ae01bff13b03', user_id:"demo", shared: false, title: "Example 2", create_date: "11/16/23", edit_date: "07/07/24", column_count: 11},
-            { id:'demo_71e11f05-dd9b-41d7-90ae-ae01bff13b04', user_id:"demo", shared: false, title: "COVID + GLP-1s", create_date: "01/01/23", edit_date: "07/07/24", column_count: 10},
-            { id:'demo_71e11f05-dd9b-41d7-90ae-ae01bff13b05', user_id:"demo", shared: false, title: "COVID + ACE2", create_date: "04/05/23", edit_date: "07/07/24", column_count: 20},
-            { id:'demo_71e11f05-dd9b-41d7-90ae-ae01bff13b06', user_id:"demo", shared: false, title: "NegCOVID + GLP-1s", create_date: "06/07/23", edit_date: "07/07/24", column_count: 25},
-            { id:'demo_71e11f05-dd9b-41d7-90ae-ae01bff13b07', user_id:"demo", shared: false, title: "NegCOVID + ACE2", create_date: "11/16/23", edit_date: "07/07/24", column_count: 11},
-            { id:'demo_71e11f05-dd9b-41d7-90ae-ae01bff13b08', user_id:"demo", shared: false, title: "Diabetes", create_date: "01/01/23", edit_date: "07/07/24", column_count: 10},
-            { id:'demo_71e11f05-dd9b-41d7-90ae-ae01bff13b09', user_id:"demo", shared: false, title: "Ashtma", create_date: "04/05/23", edit_date: "07/07/24", column_count: 20},
-            { id:'demo_71e11f05-dd9b-41d7-90ae-ae01bff13b10', user_id:"demo", shared: false, title: "COPD", create_date: "06/07/23", edit_date: "07/07/24", column_count: 25},
-            { id:'demo_71e11f05-dd9b-41d7-90ae-ae01bff13b11', user_id:"demo", shared: false, title: "opps (delete me)", create_date: "11/16/23", edit_date: "07/07/24", column_count: 11},
-            { id:'demo_71e11f05-dd9b-41d7-90ae-ae01bff13b12', user_id:"demo", shared: false, title: "Complex Stats Demo", create_date: "01/01/23", edit_date: "07/07/24", column_count: 10},
-            { id:'demo_71e11f05-dd9b-41d7-90ae-ae01bff13b13', user_id:"demo", shared: false, title: "testing 2", create_date: "04/05/23", edit_date: "07/07/24", column_count: 20},
-            { id:'demo_71e11f05-dd9b-41d7-90ae-ae01bff13b14', user_id:"demo", shared: false, title: "testing 1", create_date: "06/07/23", edit_date: "07/07/24", column_count: 25},
-            { id:'demo_71e11f05-dd9b-41d7-90ae-ae01bff13b15', user_id:"demo", shared: false, title: "test", create_date: "11/16/23", edit_date: "07/07/24", column_count: 11}
-        ],
-        "users": [
-            {
-                "id": "demo",
-                "table_count": 16
-            },
-            {
-                "id": "global",
-                "table_count": 1
-            }
-        ]
-    }
-;
-
 const parseData = (tableList) => {
     let results = {};
     results.sharedRows = tableList.tables.filter(p => p.shared).map(p => {
-        p.create_date = new Date(p.create_date);
-        p.edit_date = new Date(p.edit_date);
+        p.create_date = DateTime.fromISO(p.create_date).toJSDate();
+        p.edit_date = DateTime.fromISO(p.edit_date).toJSDate();
         return p;
     });
     results.userRows = tableList.tables.filter(p => !p.shared).map(p => {
-        p.create_date = new Date(p.create_date);
-        p.edit_date = new Date(p.edit_date);
+        p.create_date = DateTime.fromISO(p.create_date).toJSDate();
+        p.edit_date = DateTime.fromISO(p.edit_date).toJSDate();;
         return p;
     });
+    const t=1;
     return results;
 }
 
