09dda9ecd7fa64b4b803da4b620f7624a523c6ac
marc-danie <marc-danie_nazaire@hms.harvard.edu>
2024-12-06 19:59:36 -0500

    I2B2UI-955 - MVP 3- Create researcher's table of their requests - Moved location of i2b2-loader.js file

--- plugins/edu/harvard/catalyst/data-request-manager/src/index.js
+++ plugins/edu/harvard/catalyst/data-request-manager/src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import i2b2Loader from './js/i2b2-loader.js';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
