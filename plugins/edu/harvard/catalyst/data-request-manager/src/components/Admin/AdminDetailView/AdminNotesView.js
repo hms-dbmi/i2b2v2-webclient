d023b5eaa6af4b22bf70ff3ac4938222c9a79d4c
marc-danie <marc-danie_nazaire@hms.harvard.edu>
2025-01-03 15:27:47 -0500

    I2B2UI-960 - Create comments section for admin - Added sorting of admin notes

--- plugins/edu/harvard/catalyst/data-request-manager/src/components/Admin/AdminDetailView/AdminNotesView.js
+++ plugins/edu/harvard/catalyst/data-request-manager/src/components/Admin/AdminDetailView/AdminNotesView.js
@@ -3,29 +3,109 @@ import {useDispatch, useSelector} from "react-redux";
 import "./AdminNotesView.scss";
 import {
     Box, Button, Card, Paper,
-    Table, TableBody, TableCell, TableContainer, TableHead, TableRow,
+    Table, TableBody, TableCell, TableContainer, TableHead, TableRow, TableSortLabel,
     TextField,
     Typography
 } from "@mui/material";
 import Grid from "@mui/material/Grid2";
 import {getAdminNotes} from "../../../reducers/adminNotesSlice";
+import {visuallyHidden} from "@mui/utils";
 
 
 export const AdminNotesView = ({}) => {
     const dispatch = useDispatch();
     const { notes } = useSelector((state) => state.adminNotes);
+    const [order, setOrder] = React.useState('desc');
+    const [orderBy, setOrderBy] = React.useState('date');
+
+    const EnhancedTableHead = (props) => {
+        const { order, orderBy, onRequestSort } = props;
+        const headCells = [
+            {
+                id: 'date',
+                numeric: false,
+                disablePadding: false,
+                label: 'Date',
+            },
+            {
+                id: 'note',
+                numeric: false,
+                disablePadding: false,
+                label: 'Note',
+            },
+        ];
+
+        const createSortHandler = (property) => (event) => {
+            onRequestSort(event, property);
+        };
+
+        return (
+            <TableHead>
+                <TableRow>
+                    {headCells.map((headCell) => (
+                        <TableCell
+                            key={headCell.id}
+                            align={headCell.numeric ? 'right' : 'left'}
+                            padding={headCell.disablePadding ? 'none' : 'normal'}
+                            sortDirection={orderBy === headCell.id ? order : false}
+                        >
+                            <TableSortLabel
+                                active={orderBy === headCell.id}
+                                direction={orderBy === headCell.id ? order : 'asc'}
+                                onClick={createSortHandler(headCell.id)}
+                            >
+                                {headCell.label}
+                                {orderBy === headCell.id ? (
+                                    <Box component="span" sx={visuallyHidden}>
+                                        {order === 'desc' ? 'sorted descending' : 'sorted ascending'}
+                                    </Box>
+                                ) : null}
+                            </TableSortLabel>
+                        </TableCell>
+                    ))}
+                </TableRow>
+            </TableHead>
+        );
+    }
+
+    const descendingComparator = (a, b, orderBy) => {
+        if(orderBy === "date"){
+
+        }
+        if (b[orderBy] < a[orderBy]) {
+            return -1;
+        }
+        if (b[orderBy] > a[orderBy]) {
+            return 1;
+        }
+        return 0;
+    }
+
+    const getComparator = (order, orderBy) => {
+        return order === 'desc'
+            ? (a, b) => descendingComparator(a, b, orderBy)
+            : (a, b) => -descendingComparator(a, b, orderBy);
+    }
+
+    const sortedNotes = [...notes].sort(getComparator(order, orderBy));
 
     const getNotesTable = () => {
+        const handleRequestSort = (event, property) => {
+            const isAsc = orderBy === property && order === 'asc';
+            setOrder(isAsc ? 'desc' : 'asc');
+            setOrderBy(property);
+        };
+
        return ( <TableContainer className={"AdminNotesTable"} component={Paper} sx={{ maxHeight: 200 }}>
             <Table stickyHeader sx={{ minWidth: 50 }} size="small" aria-label="simple table">
-                <TableHead>
-                    <TableRow>
-                        <TableCell>Date</TableCell>
-                        <TableCell>Note</TableCell>
-                    </TableRow>
-                </TableHead>
+                <EnhancedTableHead
+                    order={order}
+                    orderBy={orderBy}
+                    onRequestSort={handleRequestSort}
+                    rowCount={notes.length}
+                />
                 <TableBody>
-                    {notes.map((row) => (
+                    {sortedNotes.map((row) => (
                         <TableRow
                             key={row.status}
                             sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
