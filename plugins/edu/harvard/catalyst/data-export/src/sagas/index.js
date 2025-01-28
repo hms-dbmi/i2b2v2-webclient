37647e743e45cebfbe1f04684111b5f8da9d40f6
marc-danie <marc-danie_nazaire@hms.harvard.edu>
2025-01-28 12:27:39 -0500

    I2B2UI-968 - Aggregate table plugin - remove request tab and redirect buttons

--- plugins/edu/harvard/catalyst/data-export/src/sagas/index.js
+++ plugins/edu/harvard/catalyst/data-export/src/sagas/index.js
@@ -1,7 +1,6 @@
 export { loadTableSaga } from "./loadTableSaga";
 export { saveTableSaga } from "./saveTableSaga";
 export { listTablesSaga } from "./listTablesSaga";
-export { makeRequestSaga } from "./makeRequestSaga";
 export { loadTermInfoSaga } from "./loadTermInfoSaga";
 export { deleteTableSaga } from "./deleteTableSaga";
 
