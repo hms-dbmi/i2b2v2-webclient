7ef4852b742ddeed07db3a8743e190f11fe4aee7
Nick Benik <nbenik@gmail.com>
2025-04-22 11:07:37 -0400

    QueryStatus: Create the table visualization module

--- js-i2b2/cells/CRC/QueryStatus/Table/Table.js
+++ js-i2b2/cells/CRC/QueryStatus/Table/Table.js
@@ -5,30 +5,89 @@ export default class Table {
             this.record = qrsRecordInfo;
             this.data = qrsData;
             this.isVisible = false;
+            this.columns = ["Breakdown Group", "Patients"];
 
-            // create the base table
+            // create the base TABLE
+            let table = d3.select(this.config.displayEl).append('table');
+            let header = table.append('thead');
+            let body = table.append('tbody');
 
-            this.config.displayEl
+            let colEls = header.append('tr')
+                .selectAll('th')
+                .data(this.columns).enter()
+                .append('th')
+                .text((column) => column);
+
+            colEls.each((d, idx, el) => {
+                let cname = "name";
+                if (idx == 1) cname = "total";
+                if (idx > 1) cname = "site";
+                el[idx].classList.add(cname);
+            });
 
         } catch(e) {
             console.error("Error in QueryStatus:Table.constructor()");
         }
     }
-    update(data) {
-        try {
 
 
 
-        } catch(e) {
-            console.error("Error in QueryStatus:Table.update()");
-        }
+    destroy() {
+        delete this.config.displayEl;
+        delete this.config;
+        delete this.record;
+        delete this.data;
+    }
+
+    update(inputData) {
+//        try {
+            if (typeof inputData === 'undefined') {
+                // no data has been set... exit
+                if (Object.keys(this.data).length === 0) return;
+            } else {
+                // get the breakdown data information (if present)
+                let resultXML = i2b2.h.XPath(inputData, "//xml_value");
+                if (resultXML.length > 0) {
+                    resultXML = resultXML[0].firstChild.nodeValue;
+                    // parse the data and put the results into the new data slot
+                    this.data = parseData(resultXML);
+                }
+            }
+
+            // select the previously created TABLE element
+            let tbody = d3.select(this.config.displayEl).select('tbody');
+            let rows = tbody.selectAll('tr').data(this.data.result);
+
+            // add new rows
+            let newRows = rows.enter().append('tr');
+            let tds = newRows.selectAll('td')
+                .data((row) => {
+                    return [row.name, row.value];
+                })
+                .enter()
+                .append('td')
+                    .text((d) => { return d; });
+
+            tds.each((d, idx, el) => {
+                let cname = "name";
+                if (idx == 1) cname = "total";
+                if (idx > 1) cname = "site";
+                el[idx].classList.add(cname);
+            });
+
+
+        // update the viewport element to the height of the visualization
+        if (this.isVisible) this.config.displayEl.parentElement.style.height = this.config.displayEl.offsetHeight + "px";
+
+        // } catch(e) {
+        //     console.error("Error in QueryStatus:Table.update()");
+        // }
     }
 
     redraw(width) {
         try {
-//            this.config.displayEl.innerHTML = "{" + this.constructor.name + "} is " + width + " pixels wide";
-            this.update();
-            return true;
+            // update the viewport element to the height of the visualization
+            if (this.isVisible) this.config.displayEl.parentElement.style.height = this.config.displayEl.offsetHeight + "px";
         } catch(e) {
             console.error("Error in QueryStatus:Table.redraw()");
         }
@@ -59,4 +118,75 @@ export default class Table {
         }
     }
 
+}
+
+let parseData = function(xmlData) {
+    let breakdown = {};
+    breakdown.result = [];
+    // process "normal" data
+    let params = i2b2.h.XPath(xmlData, 'descendant::data[@column]/text()/..');
+    // short circuit exit because there is no data
+    if (params.length === 0) return;
+    for (let i2 = 0; i2 < params.length; i2++) {
+        let entryRecord = {}
+        entryRecord.name = $('<div>').html(params[i2].getAttribute("column")).text();
+        entryRecord.value = parseInt(params[i2].firstChild.nodeValue).toLocaleString();
+
+        // format with commas
+
+        if (i2b2.PM.model.isObfuscated) {
+            const nodeValue = parseInt(params[i2].firstChild.nodeValue);
+            if (!isNaN(nodeValue) && nodeValue < 4) {
+                entryRecord.display = "< " + i2b2.UI.cfg.obfuscatedDisplayNumber.toString();
+            }
+            if (isNaN(nodeValue) || entryRecord.name === 'QueryMasterID') {
+                entryRecord.display = params[i2].firstChild.nodeValue;
+            } else {
+                entryRecord.display = entryRecord.value + "±" + i2b2.UI.cfg.obfuscatedDisplayNumber.toString();
+            }
+        }
+        if (i2b2.UI.cfg.useFloorThreshold) {
+            if (params[i2].firstChild.nodeValue < i2b2.UI.cfg.floorThresholdNumber) {
+                entryRecord.display = i2b2.UI.cfg.floorThresholdText + i2b2.UI.cfg.floorThresholdNumber.toString();
+            }
+        }
+        // Override the display value if specified by server setting the "display" attribute
+        if (typeof params[i2].attributes.display !== 'undefined') {
+            entryRecord.value = i2b2.h.Unescape(entryRecord.value);
+            entryRecord.display = params[i2].attributes.display.textContent;
+        }
+        breakdown.result.push(entryRecord);
+    }
+
+    // process "SHRINE" data
+    let ShrineNode = i2b2.h.XPath(xmlData, 'descendant::SHRINE');
+    if (ShrineNode.length) {
+        let ShrineData = {
+            complete: ShrineNode[0].getAttribute('complete'),
+            error: ShrineNode[0].getAttribute('error'),
+            sites: []
+        };
+        for (let site of i2b2.h.XPath(ShrineNode[0], "site")) {
+            let siteData = {};
+            // deal with attributes
+            for (let attrib of site.attributes) {
+                siteData[attrib.name] = attrib.value;
+            }
+            // deal with site data
+            let siteResults = i2b2.h.XPath(site, "siteresult");
+            if (siteResults.length) {
+                siteData.results = [];
+                for (let siteresult of siteResults) {
+                    siteData.results.push({
+                        name: $('<div>').html(siteresult.getAttribute('column')).text(),
+                        value: parseInt(siteresult.textContent).toLocaleString()
+                    });
+                }
+            }
+            ShrineData.sites.push(siteData);
+        }
+        breakdown.SHRINE = ShrineData;
+    }
+
+    return breakdown;
 }
