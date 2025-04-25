c468754be2bed59d72a12bd95b4e5be51dc0cea5
marc-danie <marc-danie_nazaire@hms.harvard.edu>
2025-04-25 14:58:10 -0400

    I2B2UI-1058 - Data Request Manager > Comments > Line breaks are not honored in comments and any HTML tags are automatically removed on saving the comment - Fixed line breaks not being honored

--- plugins/edu/harvard/catalyst/data-request-manager/src/sagas/getRequestCommentsSaga.js
+++ plugins/edu/harvard/catalyst/data-request-manager/src/sagas/getRequestCommentsSaga.js
@@ -6,48 +6,49 @@ import {
     GET_REQUEST_COMMENTS
 } from "../actions";
 import {getRequestCommentsError, getRequestCommentsSuccess} from "../reducers/requestCommentsSlice";
-import XMLParser from "react-xml-parser";
+import {parseXml} from "../utilities/parseXml";
 
 const getRequestCommentsRequest = (queryMasterId, comments) => {
     let data = {
         qm_key_value: queryMasterId,
     };
-    return i2b2.ajax.CRC.getQueryInstanceList_fromQueryMasterId(data).then((xmlString) => new XMLParser().parseFromString(xmlString)).catch((err) => err);
+    return i2b2.ajax.CRC.getQueryInstanceList_fromQueryMasterId(data).then((xmlString) =>parseXml(xmlString)).catch((err) => err);
 };
 
 const parseRequestCommentsXml = (exportRequestListXml) => {
     let message= "";
 
     let queryInstance = exportRequestListXml.getElementsByTagName('query_instance');
-    if (queryInstance.length > 0) {
+    if(queryInstance.length !== 0 && queryInstance[0].childNodes.length > 0){
         queryInstance = queryInstance[0];
         let queryInstanceMessage = queryInstance.getElementsByTagName('message');
-        if(queryInstanceMessage.length > 0) {
-            message = queryInstanceMessage[0].value;
+        if(queryInstanceMessage.length !== 0 && queryInstanceMessage[0].childNodes.length > 0){
+            message  = queryInstanceMessage[0].childNodes[0].nodeValue;
         }
     }
 
-    return message;
+
+return message;
 }
 
 export function* doGetRequestComments(action) {
-    const { queryMasterId  } = action.payload;
+const { queryMasterId  } = action.payload;
 
-    try {
-        const response = yield call(getRequestCommentsRequest, queryMasterId);
+try {
+const response = yield call(getRequestCommentsRequest, queryMasterId);
 
-        if (!response.error) {
-            const comments = parseRequestCommentsXml(response);
-            yield put(getRequestCommentsSuccess({comments}));
-        } else {
-            yield put(getRequestCommentsError({errorMessage: "There was an error retrieving the list of researcher data export requests"}));
-        }
-    } catch (error) {
-        yield put(getRequestCommentsError({errorMessage: "There was an error retrieving the list of researcher data export requests"}));
-    }
+if (!response.error) {
+    const comments = parseRequestCommentsXml(response);
+    yield put(getRequestCommentsSuccess({comments}));
+} else {
+    yield put(getRequestCommentsError({errorMessage: "There was an error retrieving the list of researcher data export requests"}));
+}
+} catch (error) {
+yield put(getRequestCommentsError({errorMessage: "There was an error retrieving the list of researcher data export requests"}));
+}
 }
 
 
 export function* getRequestCommentsSaga() {
-    yield takeLatest(GET_REQUEST_COMMENTS, doGetRequestComments);
+yield takeLatest(GET_REQUEST_COMMENTS, doGetRequestComments);
 }
