c19c265ab602c1af48a8a238e2b0ae5a16cc40ae
marc-danie <marc-danie_nazaire@hms.harvard.edu>
2025-07-31 09:15:41 -0400

    I2B2UI-1154 - Announcement > Hidden announcement is displayed to the user in case user is associated to only one project

--- js-i2b2/cells/PM/PM_ctrlr.js
+++ js-i2b2/cells/PM/PM_ctrlr.js
@@ -370,7 +370,7 @@ i2b2.PM._processUserConfig = function (data) {
         i2b2.PM.model.login_projectname = i2b2.h.getXNodeVal(projs[0], "name");
         try {
             let announcement = Object.entries(i2b2.PM.model.projects[i2b2.PM.model.login_project].details).find(([key, detail]) => detail.name.toUpperCase() === "ANNOUNCEMENT");
-            if (announcement.length > 0) {
+            if (announcement.length > 0 && announcement[1].status.toUpperCase() === "A") {
                 i2b2.PM.view.modal.announcementDialog.showAnnouncement(announcement[1].value);
                 return;
             }
