eedfceb8f2b842110c33a5b793433cae55afa451
marc-danie <marc-danie_nazaire@hms.harvard.edu>
2025-08-05 09:51:20 -0400

    I2B2UI-1157 - Data Request Manager > System displays incorrect tables in "Request status logs" in case response from server has not been received for the current data request

--- plugins/edu/harvard/catalyst/data-request-manager/src/reducers/requestStatusLogSlice.js
+++ plugins/edu/harvard/catalyst/data-request-manager/src/reducers/requestStatusLogSlice.js
@@ -1,14 +1,16 @@
 import {createSlice} from "@reduxjs/toolkit";
 import {REQUEST_STATUS_LOG} from "../actions";
 import {defaultState} from "../defaultState";
-import {RequestStatusLog, RequestStatusLogItem, StatusInfo} from "../models";
+import {RequestStatusLog, RequestStatusLogItem, RequestTable, StatusInfo} from "../models";
 
 export const requestStatusLogSlice = createSlice({
     name: REQUEST_STATUS_LOG,
     initialState: defaultState.requestStatusLog,
     reducers: {
         getRequestStatusLog: state => {
-            state = RequestStatusLog();
+            return RequestStatusLog({
+                isFetching: true
+            });
         },
         getRequestStatusLogSuccess: (state, { payload: { requestStatusLog } }) => {
             const statusLogs = requestStatusLog.map(requestStatus => {
@@ -19,7 +21,9 @@ export const requestStatusLogSlice = createSlice({
                     })
                 }
             })
-            state.statusLogs = statusLogs;
+            if(state.statusLogs.length === 0) {
+                state.statusLogs = statusLogs;
+            }
             state.isFetching = false;
             state.statusInfo = StatusInfo({
                 status: "SUCCESS",
