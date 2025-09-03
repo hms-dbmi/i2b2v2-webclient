a858d3f19cfeeda81eea91e87a9ace82d46574b2
marc-danie <marc-danie_nazaire@hms.harvard.edu>
2025-09-03 15:44:09 -0400

    MassCPR - Map with patient count circles - Change tile layer to CartoDB.Positron

--- js-i2b2/cells/CRC/QueryStatus/PatientCountMap/PatientCountMap.js
+++ js-i2b2/cells/CRC/QueryStatus/PatientCountMap/PatientCountMap.js
@@ -1,6 +1,8 @@
 const mapSettings = {
     "mapLayer": {
-        "urlTemplate": "http://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png",
+        "urlTemplate": 'https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png',
+        "attribution": '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
+        "subdomains": 'abcd',
         "maxZoom": 16
     }
 }
