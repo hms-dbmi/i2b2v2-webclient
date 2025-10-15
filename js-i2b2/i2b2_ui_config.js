bcea4b62d3f5abb07e62568b2233381c254006ca
marc-danie <marc-danie_nazaire@hms.harvard.edu>
2025-10-15 09:44:12 -0400

    I2B2UI-1193 - Reformat the login screen to include a message - minor edit

--- js-i2b2/i2b2_ui_config.js
+++ js-i2b2/i2b2_ui_config.js
@@ -16,7 +16,7 @@ i2b2.UI.cfg = {
     loginDefaultPassword : "demouser", /* [Default: "demouser"] Default text inside the password input */
     loginDefaultMessage : "i2b2, or Informatics for Integrating Biology and the Bedside, is a research tool that " +
         "allows users to query and analyze patient data.",
-        /* [Default: " i2b2, or Informatics for Integrating Biology and the Bedside, is a research tool that allows
+        /* [Default: "i2b2, or Informatics for Integrating Biology and the Bedside, is a research tool that allows
         users to query and analyze patient data."] Default text inside message box on the login page */
 
     obfuscatedDisplayNumber: 3, /* [Default: 3] Display number after plus/minus for obfuscated results.
