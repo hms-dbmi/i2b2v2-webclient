27472f429d319301584f9320fe4083e067a1dd52
Nick Benik <nbenik@gmail.com>
2025-11-05 13:35:42 -0500

    REFACTOR: make ONT use the new i2b2.layout.selectTab() function

--- js-i2b2/cells/ONT/ONT_view_Info.js
+++ js-i2b2/cells/ONT/ONT_view_Info.js
@@ -133,7 +133,7 @@ i2b2.ONT.view.info = {
 
         // render what we have and make the tab active if asked to do so
         i2b2.ONT.view.info.render();
-        if (display) i2b2.ONT.view.info.model.lm_view.parent.parent.setActiveContentItem(i2b2.ONT.view.info.model.lm_view.parent);
+        if (display) i2b2.layout.selectTab("i2b2.ONT.view.info");
     },
     load: function(sdxConcept, display){
         let termInfoCallback = function(response) {
