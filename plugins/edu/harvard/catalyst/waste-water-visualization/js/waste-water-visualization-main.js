9c901ef40ce5fecdd8d136926a9a4c19dc5586ba
Thomas <thomas_naughton@hms.harvard.edu>
2025-11-07 15:38:29 -0500

    working through each piece of msb plugin to see what I want to port over and refactor

--- plugins/edu/harvard/catalyst/waste-water-visualization/js/waste-water-visualization-main.js
+++ plugins/edu/harvard/catalyst/waste-water-visualization/js/waste-water-visualization-main.js
@@ -1,4 +1,16 @@
 i2b2.WasteWaterVisualization = {};
+// ---------------------------------------------------------------------------------------
+// Mock data for now
+i2b2.WasteWaterVisualization.fetchWasteWaterData = async function() {
+    // Mock API response
+    return Promise.resolve([
+        { date: "2024-10-01", viralLoad: 120 },
+        { date: "2024-10-08", viralLoad: 140 },
+        { date: "2024-10-15", viralLoad: 95 }
+    ]);
+};
+
+// ---------------------------------------------------------------------------------------
 
 window.addEventListener("I2B2_SDX_READY", (event) => {
     // drop event handlers used by this plugin
@@ -18,64 +30,69 @@ window.addEventListener("I2B2_READY", ()=> {
 
 //drop handler
 i2b2.WasteWaterVisualization.qiDropHandler = function(sdxData){
-   console.log(sdxData)
+    console.log(sdxData)
     let titleFull = sdxData.renderData.title;
     sdxData.cleanTitle = titleFull.replace('Results of', '').replace(' - FINISHED','').replace(/^\s*/gm, '');
 
-    let multiSetPSMainDiv = document.getElementById("WasteWaterVisualization-psmaindiv");
+    let wasteWaterPSMainDiv = document.getElementById("WasteWaterVisualization-psmaindiv");
 
-    multiSetPSMainDiv.innerHTML = sdxData.cleanTitle
+    wasteWaterPSMainDiv.innerHTML = sdxData.cleanTitle
 
+      // save to the model
+    i2b2.model.qiList[sdxData.sdxInfo.sdxKeyValue] = sdxData; //data packet
+    i2b2.model.qiList[sdxData.sdxInfo.sdxKeyValue].qriList = {};
 
-    console.log(titleFull)
+    // recolor the QI instances
+    let cnt = Object.keys(i2b2.model.qiList).length;
+    if (cnt < 3) cnt = 3;
+    qiColors = d3.schemeSpectral[cnt];
+    Object.values(i2b2.model.qiList).forEach((qi, idx) => qi.color = qiColors[idx] );
+    
+    // save the state
+    i2b2.state.save();
+
+    // Start a crawl to retrieve subdocuments of dropped QI's
+    i2b2.WasteWaterVisualization.parseQIXML([sdxData.sdxInfo.sdxKeyValue]);
+
+    //trigger separate render function that displays the list
+    i2b2.WasteWaterVisualization.renderQIList();
+
  
 };
+// ---------------------------------------------------------------------------------------
 
-// ---------------------------------------------------------------------------------------
-function saveState() {
-    i2b2.model.stateString = document.getElementById("stateString").value;
-    i2b2.state.save();
-}
-// ---------------------------------------------------------------------------------------
-function sendInitMsg() {
-    console.log("sending Init message...");
-    window.parent.postMessage({"msgType":"INIT"}, "/");
-}
-// ---------------------------------------------------------------------------------------
-function sendTestMsg() {
-    console.log("sending test message...");
-    let ajaxData = {
-        ont_synonym_records: "N",
-        ont_hidden_records: "N"
-    };
-    i2b2.ajax.ONT.GetCategories(ajaxData).then((data)=> {
-        console.log("Got response base from ONT.GetCategories()");
-        console.log(data);
-    })
-}
-// ---------------------------------------------------------------------------------------
-function testAuthTunnelVar() {
-    i2b2.authorizedTunnel.variable["i2b2.PM.model.isAdmin"].then((isReallyAnAdmin) => {
-        if (isReallyAnAdmin) {
-            alert("You ARE logged in as an Administrator");
-        } else {
-            alert("You are NOT logged in as an Administrator");
-        }
+i2b2.WasteWaterVisualization.renderQIList = function(){
+
+    let wasteWaterPSMainDiv = document.getElementsByClassName("WasteWaterVisualization-psmaindiv-content")[0];
+
+    //create an array to store the names of each query
+    let instanceNames = [];
+
+    //for each of the keys in the list, push an element containing the name into an array
+    Object.keys(i2b2.model.qiList).forEach(qiKeyValue => {
+        let qiColor = i2b2.model.qiList[qiKeyValue].color;
+        instanceNames.push("<div class='qi-row' data-qi-id='" + qiKeyValue + "' style='background-color:"+qiColor+"'>" + "<button class ='delete-qi'><i class='fas fa-times-circle mx-2' title='Delete'></i><span class='sr-only'>Delete</span></button>" +  i2b2.model.qiList[qiKeyValue].cleanTitle + "</div>");
     });
-}
-// ---------------------------------------------------------------------------------------
-function testAuthTunnelFunc() {
-    i2b2.authorizedTunnel.function["i2b2.h.getDomain"]().then((domain) => {
-        alert("The i2b2 domain is: "+domain);
+    if (instanceNames.length > 0){
+        document.getElementById("qi-drop-ph").classList.remove("d-block");
+        document.getElementById("qi-drop-ph").classList.add("d-none");
+    }
+    wasteWaterPSMainDiv.innerHTML = instanceNames.join("");
+
+    //delete individual QI and when we render, we re-attach handlers
+    document.querySelectorAll('.delete-qi').forEach(function(node){
+        node.addEventListener("click", function(){
+            let deleteTargetId = this.parentNode.dataset['qiId'];
+            delete i2b2.model.qiList[deleteTargetId];
+            i2b2.model.isDirty = true;
+            i2b2.state.save();
+            i2b2.WasteWaterVisualization.renderQIList();
+        });
     });
-}
+};
 
 
 
 
-// ---------------------------------------------------------------------------------------
-window.addEventListener("I2B2_READY", ()=> {
-    // the i2b2 framework is loaded and ready (including population of i2b2.model namespace)
-    if (i2b2.model.stateString === undefined) i2b2.model.stateString = "";
-    document.getElementById("stateString").value = i2b2.model.stateString;
-});
+
+
