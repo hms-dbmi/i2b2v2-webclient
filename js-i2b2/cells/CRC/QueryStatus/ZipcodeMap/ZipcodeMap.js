9a920482ec1ad6275cac210cbdca7f65e880092d
Nick Benik <nbenik@gmail.com>
2025-12-11 15:17:30 -0500

    MassCPR: QueryStatus>ZipcodeMap - Additional bug fixes to new regex processing, improved legendbox CSS

--- js-i2b2/cells/CRC/QueryStatus/ZipcodeMap/ZipcodeMap.js
+++ js-i2b2/cells/CRC/QueryStatus/ZipcodeMap/ZipcodeMap.js
@@ -5,7 +5,7 @@ let mapSettings = {
     }
 }
 const defaultZipAttrib = "ZCTA5CE10";
-const defaultZipRegEx = "^(.*)[0-9]{5}";
+const defaultZipRegEx = "^([0-9]{5}) - (.*)$";
 
 export default class ZipcodeMap {
     constructor(componentConfig, qrsRecordInfo, qrsData) {
@@ -638,13 +638,13 @@ const func_processData = (xmlData, zipRegEx) => {
         const obfuscatedNum = params[i].getAttribute("obfuscatedDisplayNumber");
         let zipSearch = zipData.match(zipRegEx);
         if (zipSearch !== null && zipSearch.length > 0) {
-            const zipCode = zipSearch[0].trim();
+            const zipCode = zipSearch[1].trim();
             const zipCount = parseInt(params[i].firstChild.nodeValue);
             const displayCount = i2b2.CRC.QueryStatus.obfuscateFloorDisplayNumber(zipCount, floorThreshold, obfuscatedNum);
             ret[zipCode] = {
                 count: zipCount,
                 display: displayCount,
-                text: zipData
+                text: zipData[0]
             };
         }
     }
