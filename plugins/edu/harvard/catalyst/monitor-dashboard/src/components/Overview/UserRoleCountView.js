743139b2c9a1cfa17c50baa53de64c63b361afa5
marc-danie <marc-danie_nazaire@hms.harvard.edu>
2026-01-15 12:19:56 -0500

    I2B2UI-1249 - Create a tile for Count of Users and Admins for "All Projects" - Added count for each data role

--- plugins/edu/harvard/catalyst/monitor-dashboard/src/components/Overview/UserRoleCountView.js
+++ plugins/edu/harvard/catalyst/monitor-dashboard/src/components/Overview/UserRoleCountView.js
@@ -1,6 +1,7 @@
 import React from "react";
 import {Box, CircularProgress, Typography} from "@mui/material";
-
+import {USER_DATA_ROLES} from "../../models";
+import "./UserRoleCountView.scss";
 
 export const UserRoleCountView = ({users, userRoleCounts, projectId}) => {
 
@@ -20,6 +21,23 @@ export const UserRoleCountView = ({users, userRoleCounts, projectId}) => {
 
         return text;
     }
+    const getRoleBreakDowns = () => {
+        const obfUsers = userRoleCounts.userRoleCountsList.filter(userRole => userRole.role === USER_DATA_ROLES.DATA_OBFSC);
+        const aggUsers = userRoleCounts.userRoleCountsList.filter(userRole => userRole.role === USER_DATA_ROLES.DATA_AGG);
+        const ldsUsers = userRoleCounts.userRoleCountsList.filter(userRole => userRole.role === USER_DATA_ROLES.DATA_LDS);
+        const deidUsers = userRoleCounts.userRoleCountsList.filter(userRole => userRole.role === USER_DATA_ROLES.DATA_DEID);
+        const protUsers = userRoleCounts.userRoleCountsList.filter(userRole => userRole.role === USER_DATA_ROLES.DATA_PROT);
+
+        return (
+            <div className={"UserRoleCountBreakdown"}>
+                <Box>Obfuscated: {obfUsers.length}</Box>
+                <Box>Aggregated: {aggUsers.length}</Box>
+                <Box>Limited Data Set: {ldsUsers.length}</Box>
+                <Box>De-identified: {deidUsers.length}</Box>
+                <Box>Protected: {protUsers.length}</Box>
+            </div>
+        )
+    }
     return (
         <Typography variant="body2" className={"UserRoleCountView ProjectOverviewInfoContentCentered"}>
             {userRoleCounts.isFetching && (
@@ -31,8 +49,10 @@ export const UserRoleCountView = ({users, userRoleCounts, projectId}) => {
                 Total Number of Users
                 <Box className={"ProjectOverviewInfoContentCount UserRoleCount"}>
                     {users.userList.length > 1 ? users.userList.length + " Users" : users.userList.length + " User"}
-                     { " including " + getAdminOrManagerCountText()}
+                    <Box>including</Box>
+                    { getAdminOrManagerCountText()}
                 </Box>
+                {getRoleBreakDowns()}
             </Box>
         </Typography>
     );
