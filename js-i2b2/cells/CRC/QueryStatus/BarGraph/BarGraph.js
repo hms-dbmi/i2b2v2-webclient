ee272fc6645e7053c4bbed8fb7b40d9e6ab47a07
Nick Benik <nbenik@gmail.com>
2026-09-10 11:11:10 -0400

    Minor improvement to QueryStatus > BarGraph

--- js-i2b2/cells/CRC/QueryStatus/BarGraph/BarGraph.js
+++ js-i2b2/cells/CRC/QueryStatus/BarGraph/BarGraph.js
@@ -17,6 +17,25 @@ export default class BarGraph {
             this.isVisible = false;
 
             if (typeof this.width === 'undefined') this.width = this.config.displayEl.parentElement.clientWidth;
+            if (this.config.advancedConfig?.maxLabelLength) {
+                this.maxLabelLength = parseInt(this.config.advancedConfig.maxLabelLength);
+            } else {
+                this.maxLabelLength = 150;
+            }
+            if (this.config.advancedConfig?.textColor) {
+                this.textColor = this.config.advancedConfig.textColor;
+            } else {
+                this.textColor = "var(--text-primary-dark)";
+            }
+            if (this.config.advancedConfig?.barColor) {
+                this.barColor = this.config.advancedConfig.barColor;
+            } else {
+                this.barColor = false;
+            }
+
+
+
+
 
             // create the SVG element
             this.height = 400 - margin.top - margin.bottom;
@@ -88,8 +107,9 @@ export default class BarGraph {
                 .attr("transform", "translate(0," + this.height + ")")
                 .call(d3.axisBottom(x))
                 .selectAll("text")
-                .text((d)=>shortenLabel(d, i2b2.CRC.cfg.params.maxBarLabelLength))
+                .text((d) => shortenLabel(d, this.maxLabelLength))
                 .attr("transform", "translate(-10,0)rotate(-45)")
+                .attr("fill", this.textColor)
                 .classed("graphLabel", true)
                 .attr("test", (x, y, z) => {
                     let h = z[y].getBoundingClientRect().height;
@@ -121,7 +141,7 @@ export default class BarGraph {
                 .classed("y-axis", true)
                 .call(d3.axisLeft(y).tickFormat(d3.format(".2~s")));
             y_axis.append("text")
-                .attr("fill","currentColor")
+                .attr("fill", this.textColor)
                 .text("Number of Patients")
                 .classed("y-label", true)
                 .attr("letter-spacing", "1.16")
@@ -130,6 +150,9 @@ export default class BarGraph {
                     let h = (height - z[0].getBoundingClientRect().width) / 2;
                     return "translate(" + l + "," + h + ") rotate(-90) ";
                 });
+            svg.selectAll(".y-axis .tick text").attr("fill", this.textColor);
+
+
 
 
             // Bars selection and updating
@@ -251,6 +274,11 @@ let parseData = function(xmlData) {
         let entryRecord = {}
         entryRecord.name = $('<div>').html(params[i2].getAttribute("column")).text();
         entryRecord.value = params[i2].firstChild.nodeValue;
+
+        // bail out if we get a -1 back from the server. This means the data is not valid.
+        if (entryRecord.value === -1) return;
+
+        // apply obfuscation
         const floorThreshold = params[i2].getAttribute("floorThresholdNumber");
         const obfuscateNumber = params[i2].getAttribute("obfuscatedDisplayNumber");
         entryRecord.display = i2b2.CRC.QueryStatus.obfuscateFloorDisplayNumber(entryRecord.value, floorThreshold, obfuscateNumber);
